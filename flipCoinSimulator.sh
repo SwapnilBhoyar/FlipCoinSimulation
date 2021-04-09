d65b1f45c2089e14a9078c571ed6492ca6f97cdb
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-09 14:12:46 +0530

    [Swapnil] added flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
+head=0
+tail=0
+while [ $head -ne 5 -a $tail -ne 5 ]
+do
 result=$(( RANDOM%2 ))
 if [[ $result -eq 0 ]]
 then
-        echo "Head wins"
+        head=$(( head+1 ))
 else
-        echo "Tail wins"
+        tail=$(( tail+1 ))
 fi
-
+done
+echo "head wins" $head "times"
+echo "tail wins" $tail "times"
