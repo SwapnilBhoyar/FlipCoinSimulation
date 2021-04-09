6d0492c90b6ee9d48be7afb3f377973e1040e694
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-09 15:48:19 +0530

    [Swapnil] added flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -13,7 +13,17 @@ fi
 done
 if [ $head -eq $tail ]
 then
-echo "it's a tie"
+	while [ $head%$tail -ne 2 -o $tail%$hrad -ne 2]
+	do
+		result=$(( RANDOM%2 ))
+		if [[ $result -eq 0 ]]
+		then
+	        	head=$(( head+1 ))
+		else
+	        	tail=$(( tail+1 ))
+		fi
+	done
+
 elif [ $head -gt $tail ]
 then
 win=$(( head-tail ))
